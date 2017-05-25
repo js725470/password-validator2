e8993e72527cae19b5397b5d1f7bc66307fe50ca
js725470 <josh-0505@hotmail.com>
2017-05-25 20:37:32 -0300

    commit stage 2

--- app/src/main/java/com/example/owner/passwordvalidator/MainActivity.java
+++ app/src/main/java/com/example/owner/passwordvalidator/MainActivity.java
@@ -10,4 +10,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static int Validate(String pass){
+        int rulesPassed=0;
+        if(!pass.equalsIgnoreCase("password"))
+        {
+            rulesPassed++;
+        }
+
+        if(pass.length()>=8)
+        {
+            rulesPassed++;
+        }
+
+        if(pass.matches(".*[0-9].*"))
+        {
+            rulesPassed++;
+        }
+
+        if(pass.matches(".*[a-z].*") && pass.matches(".*[A-Z].*"))
+        {
+            rulesPassed++;
+        }
+
+        if(pass.matches(".*[ !\"#$%&'()*+,-./:;<=>?@[\\\\]^_`{|}~].*"))
+        {
+            rulesPassed++;
+        }
+        return rulesPassed;
+    }
 }
--- app/src/test/java/com/example/owner/passwordvalidator/ExampleUnitTest.java
+++ app/src/test/java/com/example/owner/passwordvalidator/ExampleUnitTest.java
@@ -14,4 +14,51 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    private String test1 = "password";
+    private String test2 = "PaSsWoRd";
+    private String test3 = "aaaaaaa";
+    private String test4 = "aaaaaaaa";
+    private String test5 = "aaaaaaaaa";
+    private String test6 = "";
+    private String test7 = "a1a";
+    private String test8 = "password8";
+    private String test9 = "pa";
+    private String test10 = "PA";
+    private String test11 = "Pa";
+    private String test12 = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"; //allowed
+    private String test13 = " aA1!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+    @Test
+    public void TestPassword() throws Exception {
+        assertEquals(1, MainActivity.Validate(test1));
+        assertEquals(2, MainActivity.Validate(test2));
+    }
+
+    @Test
+    public void TestLength() throws Exception {
+        assertEquals(1, MainActivity.Validate(test3));
+        assertEquals(2, MainActivity.Validate(test4));
+        assertEquals(2, MainActivity.Validate(test5));
+        assertEquals(1, MainActivity.Validate(test6));
+    }
+
+    @Test
+    public void TestDigit() throws Exception{
+        assertEquals(2, MainActivity.Validate(test7));
+        assertEquals(3, MainActivity.Validate(test8));
+    }
+
+    @Test
+    public void TestUpperAndLower() throws Exception{
+        assertEquals(1, MainActivity.Validate(test9));
+        assertEquals(1, MainActivity.Validate(test10));
+        assertEquals(2, MainActivity.Validate(test11));
+    }
+
+    @Test
+    public void TestSpecChar() throws Exception{
+        assertEquals(3, MainActivity.Validate(test12));
+        assertEquals(5, MainActivity.Validate(test13));
+    }
 }
