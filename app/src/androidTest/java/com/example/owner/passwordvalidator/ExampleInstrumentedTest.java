4c0464c9fe5ff66dac4fe9d410af321c58a3d396
js725470 <josh-0505@hotmail.com>
2017-05-28 15:32:01 -0300

    commit stage 3

--- app/src/androidTest/java/com/example/owner/passwordvalidator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/owner/passwordvalidator/ExampleInstrumentedTest.java
@@ -2,8 +2,14 @@ package com.example.owner.passwordvalidator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.action.ViewActions.*;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,5 +28,26 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.owner.passwordvalidator", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void passwordNotStrongTest() throws Exception {
+        onView(withId(R.id.edit)).perform(replaceText(""));
+        onView(withId(R.id.edit)).perform(typeText("password"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.strength)).check(matches(withText("Password not strong.")));
+        //onView(withId(R.id.strength)).check(matches(withText("Password is strong.")));
+    }
+
+    @Test
+    public void passwordStrongTest() throws Exception {
+        onView(withId(R.id.edit)).perform(replaceText(""));
+        onView(withId(R.id.edit)).perform(typeText("passwordP1?"));
+        onView(withId(R.id.button)).perform(click());
+        //onView(withId(R.id.strength)).check(matches(withText("Password not strong.")));
+        onView(withId(R.id.strength)).check(matches(withText("Password is strong.")));
     }
 }
--- app/src/main/java/com/example/owner/passwordvalidator/MainActivity.java
+++ app/src/main/java/com/example/owner/passwordvalidator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.owner.passwordvalidator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private EditText replace;
+    private TextView display;
+    private int num;
+
+    public void buttonOnClick(View v){
+        replace = (EditText) findViewById(R.id.edit);
+        num = Validate(replace.getText().toString());
+        display = (TextView) findViewById(R.id.strength);
+
+
+        if(num>=0 && num<5){
+            display.setText("Password not strong.");
+        }
+        else if(num == 5){
+            display.setText("Password is strong.");
+        }
+        else{
+            display.setText("Error. Try again.");
+        }
+        //display.setText(replace.getText());
+    }
+
+    public static int Validate(String pass){
+        int rulesPassed=0;
+        if(!pass.equalsIgnoreCase("password"))
+        {
+            rulesPassed++;
+        }
+
+        if(pass.length()>=8)
+        {
+            rulesPassed++;
+        }
+
+        if(pass.matches(".*[0-9].*"))
+        {
+            rulesPassed++;
+        }
+
+        if(pass.matches(".*[a-z].*") && pass.matches(".*[A-Z].*"))
+        {
+            rulesPassed++;
+        }
+
+        if(pass.matches(".*[ !\"#$%&'()*+,-./:;<=>?@[\\\\]^_`{|}~].*"))
+        {
+            rulesPassed++;
+        }
+        return rulesPassed;
     }
 }
